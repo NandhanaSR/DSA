d7140c830b1908b2d5181b5de1f1d6eddfa8eb2c
NandhanaSR <81952428+NandhanaSR@users.noreply.github.com>
2025-06-30 18:47:04 +0530

    Update Two_Sum.c

--- Two_Sum.c
+++ Two_Sum.c
@@ -1,3 +1,8 @@
+// Given an array of integers nums and an integer target, return the indices i and j such that nums[i] + nums[j] == target and i != j.
+// You may assume that every input has exactly one pair of indices i and j that satisfy the condition.
+// Return the answer with the smaller index first.
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "dynamic_input.c"

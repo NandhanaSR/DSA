c06fa14219adbc331ee8f9cf398fa0cf5a363ddd
NandhanaSR <81952428+NandhanaSR@users.noreply.github.com>
2025-06-30 18:48:45 +0530

    Update Products_of_Array_Except_Self.c

--- Products_of_Array_Except_Self.c
+++ Products_of_Array_Except_Self.c
@@ -1,3 +1,7 @@
+// Given an integer array nums, return an array output where output[i] is the product of all the elements of nums except nums[i].
+// Each product is guaranteed to fit in a 32-bit integer.
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "dynamic_input.c"
